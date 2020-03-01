6e59dc7777774e0027aae815c9184b33d6e0f55f
bjornhorst <bjornvdhorst10@gmail.com>
2020-03-01 14:12:10 +0100

    1

---
+++ XmlCrud/App_Code/SongMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace XmlCrud.App_Code
+{
+    public class SongMethods
+    {
+        public DataSet GetAllSongs(string file)
+        {
+            //setup some datesets, tables and columns
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dtSongs = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcYear = new DataColumn("year");
+            DataColumn dcGerne = new DataColumn("gerne");
+            DataColumn dcTime = new DataColumn("time");
+            DataColumn dcFile = new DataColumn("file");
+
+            //connect objects to datasets, tables and columns
+            dtSongs.Columns.Add(dcId);
+            dtSongs.Columns.Add(dcArtist);
+            dtSongs.Columns.Add(dcTitle);
+            dtSongs.Columns.Add(dcYear);
+            dtSongs.Columns.Add(dcGerne);
+            dtSongs.Columns.Add(dcTime);
+            dtSongs.Columns.Add(dcFile);
+
+            //Read the data of XML file
+            ds.ReadXml(file);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath(file));
+
+            return ds;
+        }
+    }
+}
